b83d67eb39d22353725616d9a60b3a61caba3c2f
Alexander Blankenstein <a.blankenstein@live.com>
2022-08-16 10:06:52 -0400

    Setup environment. Bring over stopwatch activity code.

---
+++ app/src/main/java/com/example/stopwatch_fragment/StopwatchFragment.java
@@ -0,0 +1,98 @@
+package com.example.stopwatch_fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class StopwatchFragment extends Activity {
+
+    //number of seconds displayed on the stopwatch
+    private int seconds = 0;
+    //is the stopwatch running?
+    private boolean running;
+    //was stopwatch running before onStop method call?
+    private boolean wasRunning;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+        //If app was destroyed prior, reload from saved state.
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    //If activity is paused, stop the stopwatch
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    //If stopwatch was running when it was paused, then set it to running again.
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (wasRunning) {
+           running = true;
+        }
+    }
+
+    //Start the stopwatch running when the Start button is clicked.
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    //Stop the stopwatch running when the Stop button is clicked
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    //Reset the stopwatch when the Reset button
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    //Sets the number of seconds on the timer
+    private void runTimer() {
+        final TextView timeView = findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+
+        //Update the stopwatch using a handler with a delay of 1 second
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+
+                //Delay post so that display only changes after a second.
+                // This makes it not very accurate but shows example of a delay.
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+}
