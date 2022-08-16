9ccbb952c2bf057ee2f7ff8c56c927cdacfd9b84
Alexander Blankenstein <a.blankenstein@live.com>
2022-08-16 10:44:25 -0400

    changed fragment to framelayout and add fragment transaction to resume app when destroyed.

--- app/src/main/java/com/example/stopwatch_fragment/TopActivity.java
+++ app/src/main/java/com/example/stopwatch_fragment/TopActivity.java
@@ -1,6 +1,7 @@
 package com.example.stopwatch_fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +11,13 @@ public class TopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top);
+        if (savedInstanceState == null) {
+            StopwatchFragment stopwatch = new StopwatchFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.stopwatch_container, stopwatch);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        }
     }
 }
